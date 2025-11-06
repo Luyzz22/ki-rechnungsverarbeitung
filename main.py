f5433b999c58c2e71c84c8475ec090b53a1de62f
Luis Schenk <luis@schenk.com>
2025-11-06 13:13:03 +0000

    v4.3 - Landing Page added

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, responses
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "web" / "static"
+
+# statische Dateien (Logo, CSS usw.)
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+@app.get("/landing", response_class=responses.HTMLResponse)
+async def landing_page():
+    index_path = STATIC_DIR / "landing" / "index.html"
+    return responses.FileResponse(index_path)
